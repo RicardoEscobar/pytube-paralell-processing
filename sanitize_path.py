f1bf6e234baa7d19e35a4214e8a56a69c3179900
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-05-08 22:57:42 -0600

    Add sanitize_path.py

---
+++ sanitize_path.py
@@ -0,0 +1,7 @@
+"""This clears a string of characters that are not allowed in a file path."""
+import re
+
+
+def sanitize_path(path: str) -> str:
+    """This clears a string of characters that are not allowed in a file path."""
+    return re.sub(r'[<>:"/\\|?*]', '', path)

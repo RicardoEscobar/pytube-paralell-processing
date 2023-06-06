960f6dee513d08792dcf31c5b5c68fb42c3ce04a
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-06-05 18:07:30 -0600

    Refactor download_video_hq function out of: controller\pytube3.py

--- controller/pytube3.py
+++ controller/pytube3.py
@@ -1,5 +1,4 @@
 import multiprocessing as mp
-import os
 from pathlib import Path
 from pytube import Playlist, YouTube
 
@@ -9,47 +8,10 @@ from controller.to_mp3 import to_mp3
 
 
 @time_it
-def download_video_hq(youtube_url: str, output_path: Path = Path()):
-    """Download a youtube video at the highest quality."""
-    # Create YouTube object from the url string.
-    yt = YouTube(youtube_url)
-    
-    # Get temporal directory for downloading the streams.
-    tmp_path = Path(output_path / 'tmp')
-
-    # get all DASH video streams
-    dash_streams = yt.streams.filter(type='video', subtype='mp4', progressive=False)
-
-    # sort by resolution or bitrate to get the highest quality stream
-    highest_quality = dash_streams.order_by('resolution').desc().first()
-
-    # download the stream
-    video_stream = highest_quality.download(output_path=tmp_path)
-
-    # get all DASH audio streams
-    dash_streams = yt.streams.filter(type='audio', subtype='webm', progressive=False)
-
-    # sort by bitrate to get the highest quality stream
-    highest_quality = dash_streams.order_by('abr').desc().first()
-
-    # download the stream
-    audio_stream = highest_quality.download(output_path=tmp_path)
-
-    # Merge the audio and video streams
-    output_file_path = Path(output_path, sanitize_path(yt.title) + '.mp4')
-    output_file_path = output_file_path.resolve()
-    
-    os.system(f'ffmpeg -hide_banner -loglevel error -i "{video_stream}" -i "{audio_stream}" -c copy "{output_file_path}"')
-
-    # Delete the temporal files
-    Path(video_stream).unlink()
-    Path(audio_stream).unlink()
-
-@time_it
 def download_audio_yt(youtube_url: str, output_path: Path = Path()):
     """Download a youtube audio."""
     # Create YouTube object from the url string.
-    yt = YouTube(youtube_url)    
+    yt = YouTube(youtube_url)
     
     # Create tmp directory for downloading the streams.
     tmp_path = Path(output_path / 'tmp')

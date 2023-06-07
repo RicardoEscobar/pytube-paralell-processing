f762a7682a7a374cd68286b44be6ffa9aa6d9d57
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-06-06 20:40:34 -0600

    Add on_progress and on_complete functions to: controller\download_audio_yt.py

--- controller/download_audio_yt.py
+++ controller/download_audio_yt.py
@@ -6,25 +6,67 @@ from pytube import YouTube
 
 from controller.time_it import time_it
 from controller.to_mp3 import to_mp3
+from functools import partial
 
 
 @time_it
-def download_audio_yt(youtube_url: str, output_path: Path = Path()):
+def download_audio_yt(
+    youtube_url: str,
+    output_path: str = None,
+    on_progress_callback=None,
+    on_complete_callback=None,
+):
     """Download a youtube audio."""
-    # Create YouTube object from the url string.
-    youtube = YouTube(youtube_url)
+
+    # Create tmp directory for downloading the streams.
+    if output_path is None:
+        output_path = Path("." / "downloads")
+    else:
+        output_path = Path(output_path)
 
     # Create tmp directory for downloading the streams.
     tmp_path = Path(output_path / "tmp")
     tmp_path.mkdir(exist_ok=True)
 
-    # get all DASH audio streams
-    filename = (
-        youtube.streams.filter(only_audio=True)
+    # Create Stream object from the url string.
+    stream = (
+        YouTube(youtube_url)
+        .streams.filter(only_audio=True)
         .order_by("abr")
         .desc()
         .first()
-        .download(output_path=tmp_path)
     )
 
-    to_mp3(Path(filename), output_path)
+    # Generate actual callback function with 'partial', to pass the bytes_total argument.
+    partial_on_progress_callback = partial(
+        on_progress_callback, bytes_total=stream.filesize, filename=stream.title
+    )
+
+    # Create YouTube object from the url string.
+    youtube = YouTube(
+        youtube_url,
+        on_progress_callback=partial_on_progress_callback,
+        on_complete_callback=on_complete_callback,
+    )
+
+    # Download the audio
+    try:
+        file_path = (
+            youtube.streams.filter(only_audio=True)
+            .order_by("abr")
+            .desc()
+            .first()
+            .download()
+        )
+
+        # Make a Path object from the file_path string.
+        file_path = Path(file_path)
+    except Exception as e:
+        print(type(e).__name__)
+        print(e)
+        import traceback
+
+        traceback.print_tb(e.__traceback__)
+    else:
+        # Convert to mp3
+        to_mp3(Path(file_path), output_path)

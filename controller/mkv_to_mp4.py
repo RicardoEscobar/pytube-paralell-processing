787c37b554c9710b5018560568a58db7fb62b6e7
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-06-06 19:05:01 -0600

    Format controller\mkv_to_mp4.py

--- controller/mkv_to_mp4.py
+++ controller/mkv_to_mp4.py
@@ -4,6 +4,7 @@ import multiprocessing as mp
 import os
 from controller.time_it import time_it
 
+
 @time_it
 def mkv_to_mp4(input_video_path: str, output_video_file: str = None) -> str:
     """Convert a mkv file to a mp4 file.
@@ -15,7 +16,7 @@ def mkv_to_mp4(input_video_path: str, output_video_file: str = None) -> str:
     input_video_path = Path(input_video_path)
 
     if output_video_file is None:
-        output_video_file = input_video_path.with_suffix('.mp4')
+        output_video_file = input_video_path.with_suffix(".mp4")
 
     # ffmpeg_command = f'ffmpeg -hide_banner -loglevel error -i "{input_video_path}" -c:v libx264 -preset:v slow -crf 18 -r 60 -vf "scale=-2:1080,format=yuv420p" -c:a aac -b:a 192k -ac 2 -ar 48000 -movflags +faststart -n "{output_video_file}"'
     ffmpeg_command = f'ffmpeg -hide_banner -loglevel error -i "{input_video_path}" -c:v libx264 -c:a copy -n "{output_video_file}"'
@@ -28,17 +29,19 @@ def mkv_to_mp4(input_video_path: str, output_video_file: str = None) -> str:
         return None
     else:
         # Delete the input file.
-        input_video_path.unlink()            
+        input_video_path.unlink()
         return output_video_file
 
-def get_mkv_file(input_dir: str = '.') -> str:
+
+def get_mkv_file(input_dir: str = ".") -> str:
     """Generator that yields of all mkv files in a directory and sub directories."""
     input_dir = Path(input_dir)
     for file in input_dir.iterdir():
         if file.is_dir():
             yield from get_mkv_file(file)
-        elif file.suffix == '.mkv':
+        elif file.suffix == ".mkv":
             yield file
+
 
 @time_it
 def process_dir(input_dir: str) -> None:
@@ -49,15 +52,22 @@ def process_dir(input_dir: str) -> None:
 
     # Use pool to parallelize the conversion.
     pool = mp.Pool()
-    pool.starmap(mkv_to_mp4, [(file.resolve(), file.with_suffix('.mp4')) for file in get_mkv_file(input_dir)])
+    pool.starmap(
+        mkv_to_mp4,
+        [
+            (file.resolve(), file.with_suffix(".mp4"))
+            for file in get_mkv_file(input_dir)
+        ],
+    )
 
 
 @time_it
 def main():
-    PATH = Path(r'E:\grabaciones')
+    PATH = Path(r"E:\grabaciones")
     process_dir(PATH)
-    #output_video_file = r'E:\grabaciones\Carhartt\2023-05-29\2023-05-29_12-00-27-compressed.mp4'
-    #mkv_to_mp4(PATH, output_video_file)
+    # output_video_file = r'E:\grabaciones\Carhartt\2023-05-29\2023-05-29_12-00-27-compressed.mp4'
+    # mkv_to_mp4(PATH, output_video_file)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

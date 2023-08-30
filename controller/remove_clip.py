e74c2c2c4c7c57ab28f72ea6f7fbbb90b4600e5d
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-08-30 15:26:12 -0600

    Refactor and change of URL for the example video at: controller\remove_clip.py

--- controller/remove_clip.py
+++ controller/remove_clip.py
@@ -1,18 +1,21 @@
 """This script removes a section of the video given a start and an end time."""
 
 # If this file runs as a script, run add the parent directory to the sys.path.
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
     from pathlib import Path
+
     sys.path.append(str(Path(__file__).resolve().parent.parent))
-    
+
 from pathlib import Path
 import os
 from controller.time_it import time_it
 
 
 @time_it
-def remove_clip(input_video_path: str, start_time: str, end_time: str, output_video_file: str = None) -> str:
+def remove_clip(
+    input_video_path: str, start_time: str, end_time: str, output_video_file: str = None
+) -> str:
     """Given a start and an end time, remove that section of the video.
     args:
         video_file: Path to the video file.
@@ -22,13 +25,13 @@ def remove_clip(input_video_path: str, start_time: str, end_time: str, output_vi
         Path to the output video file.
     """
     input_video_path = Path(input_video_path)
-        
+
     # Use the same video file name as input, but change the extension to .mp4
     if output_video_file is None:
-        output_video_file = input_video_path.with_suffix('.mp4')
+        output_video_file = input_video_path.with_suffix(".mp4")
     else:
         output_video_file = Path(output_video_file)
-    
+
     ffmpeg_command = f'ffmpeg -hide_banner -loglevel error -i "{input_video_path}" -c copy -ss {start_time} -to {end_time} -n "{output_video_file}"'
     print(ffmpeg_command)
 
@@ -40,21 +43,26 @@ def remove_clip(input_video_path: str, start_time: str, end_time: str, output_vi
     else:
         # input_video_path.unlink() # Removed temporarily for testing.
         return output_video_file.resolve()
-    
-def get_video_file(input_dir: str = '.') -> str:
+
+
+def get_video_file(input_dir: str = ".") -> str:
     """Generator that yields of all video files in a directory."""
     input_dir = Path(input_dir)
     for file in input_dir.iterdir():
-        if file.suffix in ['.mp4', '.mkv']:
+        if file.suffix in [".mp4", ".mkv"]:
             yield file
+
 
 @time_it
 def main():
-    VIDEO = r'E:\grabaciones\Carhartt\2023-07-14\2023-07-14_11-20-56.mkv'
-    START_TIME = '00:00:00'
-    END_TIME = '00:55:33'
-    OUTPUT_VIDEO_FILE = r'E:\grabaciones\Carhartt\2023-07-14\2023-07-14_11-20-56_clip.mkv'
+    VIDEO = r"E:\grabaciones\Carhartt\2023-08-30\2023-08-30_14-02-12.mkv"
+    START_TIME = "00:00:00"
+    END_TIME = "01:00:13"
+    OUTPUT_VIDEO_FILE = (
+        r"E:\grabaciones\Carhartt\2023-07-14\2023-08-30_14-02-12_clip.mkv"
+    )
     remove_clip(VIDEO, START_TIME, END_TIME, OUTPUT_VIDEO_FILE)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

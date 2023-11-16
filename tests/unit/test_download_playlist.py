3d5b535cfa810f1f67e1a0a5e58a270697d9ba83
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-11-16 01:24:35 -0600

    Add root folder to path for unit tests

--- tests/unit/test_download_playlist.py
+++ tests/unit/test_download_playlist.py
@@ -1,3 +1,10 @@
+# Add root folder to path
+import sys
+from pathlib import Path
+
+project_directory = Path(__file__).resolve().parents[2]
+sys.path.append(str(project_directory))
+
 import unittest
 from controller.download_playlist import download_playlist
 
@@ -7,10 +14,10 @@ class TestDownloadPlaylist(unittest.TestCase):
 
     def test_download_playlist(self):
         """test_download_playlist"""
-        url = "https://youtube.com/playlist?list=PLzuJYcABcR-TXJ2jy5xX-MvmiO-YIRiag&si=sWwC1EPvjiqL_KvC"
+        url = "https://youtube.com/playlist?list=PLzuJYcABcR-TXJ2jy5xX-MvmiO-YIRiag&si=y7U-DUDPTv5yVPjb"
         output_dir = r"E:\YouTube downloads"
 
-        result = download_playlist(url, output_dir)
+        result = download_playlist(url, output_dir, start=0)
         for path in result:
             print(path)
 

29484a394b39d2484f3e7e071c9d50812c5cfab9
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-11-16 01:24:19 -0600

    Add start and end index parameters to download_playlist function

--- controller/download_playlist.py
+++ controller/download_playlist.py
@@ -15,19 +15,26 @@ from controller.download_video_hq import download_video_hq
 
 
 @time_it
-def download_playlist(playlist_url: str, output_dir: str = None) -> List[str]:
+def download_playlist(
+    playlist_url: str, output_dir: str = None, start: int = 0, end: int = None
+) -> List[str]:
     """
     Download a youtube playlist.
 
     Args:
         playlist_url (str): The URL of the playlist to download.
         output_dir (str): The path to the output directory.
+        start (int): The index of the first video to download.
+        end (int): The index of the last video to download.
     Returns:
         A list of the paths of the downloaded videos.
     """
 
     # Create Playlist object from the url string.
     playlist = Playlist(playlist_url)
+
+    # Print how many videos are in the playlist.
+    print(f"Number of videos in playlist: {len(playlist.video_urls)}")
 
     # Get the output directory.
     output_dir = Path(output_dir)
@@ -37,8 +44,12 @@ def download_playlist(playlist_url: str, output_dir: str = None) -> List[str]:
     playlist_dir = Path(output_dir / sanitize_path(playlist.title))
     playlist_dir.mkdir(parents=True, exist_ok=True)
 
+    # Set the end index to the length of the playlist if it is None.
+    if end is None:
+        end = len(playlist.video_urls)
+
     # Get the URLs of all the videos in the playlist.
-    video_urls = playlist.video_urls
+    video_urls = playlist.video_urls[start:end]
 
     # Download the videos in parallel.
     with concurrent.futures.ThreadPoolExecutor() as executor:

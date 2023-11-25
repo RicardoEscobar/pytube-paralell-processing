12384fa9953380677cd61f7f1ad0429c65c2cadd
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-11-25 13:10:07 -0600

    Update playlist URL in test_download_playlist

--- tests/unit/test_download_playlist.py
+++ tests/unit/test_download_playlist.py
@@ -14,10 +14,10 @@ class TestDownloadPlaylist(unittest.TestCase):
 
     def test_download_playlist(self):
         """test_download_playlist"""
-        url = "https://youtube.com/playlist?list=PLzuJYcABcR-TXJ2jy5xX-MvmiO-YIRiag&si=y7U-DUDPTv5yVPjb"
+        url = "https://youtube.com/playlist?list=PLzuJYcABcR-TXJ2jy5xX-MvmiO-YIRiag&si=BXDQ-BGu1ttAcGJu"
         output_dir = r"E:\YouTube downloads"
 
-        result = download_playlist(url, output_dir, start=0)
+        result = download_playlist(url, output_dir)
         for path in result:
             print(path)
 

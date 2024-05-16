1272de3aab2ee4cf4718a5c805c2e6a74983d61e
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2024-05-16 02:12:19 +0000

    Refactor download_playlist.py for improved flexibility and maintainability

--- controller/download_playlist.py
+++ controller/download_playlist.py
@@ -7,7 +7,7 @@ import concurrent.futures
 from pathlib import Path
 
 from pytube import Playlist
-from typing import List
+from typing import List, Union
 
 from controller.sanitize_path import sanitize_path
 from controller.time_it import time_it
@@ -16,37 +16,56 @@ from controller.download_video_hq import download_video_hq
 
 @time_it
 def download_playlist(
-    playlist_url: str, output_dir: str = None, start: int = 0, end: int = None
+    playlist_url: str,
+    output_dir: Union[str, Path] = ".",
+    start: int = 0,
+    end: int = None,
+    show_index: bool = False,
 ) -> List[str]:
     """
     Download a youtube playlist.
 
     Args:
         playlist_url (str): The URL of the playlist to download.
-        output_dir (str): The path to the output directory.
+        output_dir (str, Path): The path to the output directory.
         start (int): The index of the first video to download.
         end (int): The index of the last video to download.
+        show_index (bool): Whether to include the index on the playlist at the
+        start of the filename.
     Returns:
         A list of the paths of the downloaded videos.
     """
-
     # Create Playlist object from the url string.
     playlist = Playlist(playlist_url)
 
-    # Print how many videos are in the playlist.
-    print(f"Number of videos in playlist: {len(playlist.video_urls)}")
+    # Create the index for the videos if required.
+    if show_index:
+        # Save playlist indexes to a list.
+        playlist_indexes = list(range(1, len(playlist.video_urls) + 1))
+    else:
+        playlist_indexes = [None] * len(playlist.video_urls)
 
-    # Get the output directory.
-    output_dir = Path(output_dir)
-    output_dir = output_dir.resolve()
-
-    # Create a directory for the playlist.
-    playlist_dir = Path(output_dir / sanitize_path(playlist.title))
-    playlist_dir.mkdir(parents=True, exist_ok=True)
+    # Set the start index to 0 if it is None.
+    if start is None:
+        start = 0
 
     # Set the end index to the length of the playlist if it is None.
     if end is None:
         end = len(playlist.video_urls)
+
+    # If the output directory is a string, convert it to a Path object.
+    if isinstance(output_dir, str):
+        output_dir = Path(output_dir)
+
+    # If the output directory does not exist, create it.
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Print how many videos are in the playlist.
+    print(f"Number of videos in playlist: {len(playlist.videos)}")
+
+    # Create a directory for the playlist
+    playlist_name = playlist.title + " [" + playlist.owner + "]"
+    playlist_path = output_dir / sanitize_path(playlist_name)
 
     # Get the URLs of all the videos in the playlist.
     video_urls = playlist.video_urls[start:end]
@@ -54,7 +73,10 @@ def download_playlist(
     # Download the videos in parallel.
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results = executor.map(
-            download_video_hq, video_urls, [playlist_dir] * len(video_urls)
+            download_video_hq,
+            video_urls,
+            [playlist_path] * len(video_urls),
+            playlist_indexes,
         )
 
     # Return the paths of the downloaded videos.
